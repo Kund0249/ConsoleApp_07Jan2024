659fb2f2468633140306a5825014796a2c24cfb7
Kund0249 <k.kundan1999@gmail.com>
2024-02-17 15:19:31 +0530

    Class and Object

---
+++ ConsoleApp_07Jan2024/StaticMember/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_07Jan2024.StaticMember
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            Student S1 = new Student()
+            {
+                RollNo = 1,
+                Name = "A"
+            };
+
+            Student S2 = new Student()
+            {
+                RollNo = 2,
+                Name = "B"
+            };
+
+            Student S3 = new Student()
+            {
+                RollNo = 3,
+                Name = "B"
+            };
+
+            Console.WriteLine("Total Student : " + Student.Count);
+        }
+    }
+}
---
+++ ConsoleApp_07Jan2024/StaticMember/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_07Jan2024.StaticMember
+{
+   public class Student
+    {
+        //Instance Member
+        public int RollNo;
+        public string Name;
+
+        //Static Member
+        public static int Count;
+        public Student()
+        {
+            Count++;
+        }
+
+        static Student()
+        {
+            Count = 0;
+        }
+    }
+}
